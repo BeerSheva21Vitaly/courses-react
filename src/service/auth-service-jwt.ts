5feae59254cab5d754fbd5be88853e299776eda8
vitaly lukyanchenko <vit123@list.ru>
2022-01-16 16:12:20 +0200

    adds auth class implementation (getUserData)

--- src/service/auth-service-jwt.ts
+++ src/service/auth-service-jwt.ts
@@ -2,10 +2,31 @@ import {LoginData} from "../models/common/login-data";
 import {UserData} from "../models/common/user-data";
 import { Observable } from "rxjs";
 import AuthService from "./auth-service";
+import { AUTH_TOKEN } from "./courses-service-rest";
+
+const pollingInterval: number = 2000;
+const nonAuthorizedUser: UserData = {
+    username: "",
+    isAdmin: false,
+    displayName: "Non authorized user"
+}
 
 export default class AuthServiceJwt implements AuthService {
+    private cache: string = "";
     getUserData(): Observable<UserData> {
-        throw new Error("Method not implemented.");
+        return new Observable<UserData>(subscriber => {
+            let userData: UserData = fetchUserData();
+            this.cache = JSON.stringify(userData);
+            subscriber.next(userData);
+            setInterval(() => {
+                userData = fetchUserData();
+                const userDataJson = JSON.stringify(userData);
+                if(userDataJson !== this.cache) {
+                    subscriber.next(userData);
+                    this.cache = userDataJson;
+                }
+            }, pollingInterval)
+        })
     }
     login(loginData: LoginData): Promise<boolean> {
         throw new Error("Method not implemented.");
@@ -15,3 +36,23 @@ export default class AuthServiceJwt implements AuthService {
     }
 
 }
+
+function fetchUserData(): UserData {
+    const token: string | null = localStorage.getItem(AUTH_TOKEN);
+    if(typeof token !== "string") {
+        return nonAuthorizedUser;
+    } else {
+        return tokenToUserData(token);
+    }
+}
+
+function tokenToUserData(token: string): UserData {
+    const rawPayLoad = token.split(".")[1] ; //JSON in base64
+    const payLoad: any = Buffer.from(rawPayLoad, "base64");
+    return {
+        username: payLoad.username,
+        isAdmin: payLoad.isAdmin,
+        displayName: payLoad.displayName,
+    }
+}
+
